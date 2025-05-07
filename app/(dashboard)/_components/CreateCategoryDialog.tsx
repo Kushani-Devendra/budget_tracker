b16db8d70776b5cb4681a34676c7dae0afc4f053
Kushani Sasiprabha <kushani2000@gmail.com>
2025-05-07 11:00:13 +0530

    added changes

---
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+interface Props {}
+
+function CreateCategoryDialog() {
+  return <div>CreateCategoryDialog</div>;
+}
+
+export default CreateCategoryDialog;
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,6 +3,7 @@ import { prisma } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
+import CreateTransactionDialog from "./_components/CreateTransactionDialog";
 
 async function page() {
   const user = await currentUser();
@@ -28,18 +29,29 @@ async function page() {
           <p className="text-3xl font-bold">Hello, {user.firstName}!👋 </p>
 
           <div className="flex items-center gap-3">
-            <Button
-              variant={"outline"}
-              className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
-            >
-              New income
-            </Button>
-            <Button
-              variant={"outline"}
-              className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white"
-            >
-              New expense .
-            </Button>
+            <CreateTransactionDialog
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="border-emerald-500 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
+                >
+                  New income
+                </Button>
+              }
+              type="income"
+            />
+
+            <CreateTransactionDialog
+              trigger={
+                <Button
+                  variant={"outline"}
+                  className="border-rose-500 bg-rose-950 text-white hover:bg-rose-700 hover:text-white"
+                >
+                  New expense
+                </Button>
+              }
+              type="expense"
+            />
           </div>
         </div>
       </div>
